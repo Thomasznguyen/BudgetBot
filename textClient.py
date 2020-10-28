13afd9a963fd251341c13af46a6fbb0c1b6c769a
Tommy Nguyen <57327326+Thomasznguyen@users.noreply.github.com>
2020-10-28 11:40:31 -0400

    V1.02
    Updated Heroku Files

--- textClient.py
+++ textClient.py
@@ -1,12 +1,18 @@
 from twilio.rest import Client
-from config import auth_token,account_sid
+from config import auth_token, account_sid
 
-client = Client(account_sid,auth_token)
-def sendSMS(number,message):
+client = Client(account_sid, auth_token)
+
+
+def sendSMS(number, message):
     print(message)
     message = client.messages.create(
         to=number,
-        from_="Twilio Accounts Phone Number Goes Here",
+        from_="3012152256",
         body=message
     )
     print(message.sid)
+
+
+def getSMS():
+    message = client.messages.get()
